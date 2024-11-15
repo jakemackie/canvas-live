97eb9dd93f381410f1116a210a17b5896b035188
Jake Mackie <jake@hiyield.co.uk>
2024-11-15 17:12:39 +0000

    feat: Nice gradient blobs

--- app/page.tsx
+++ app/page.tsx
@@ -33,18 +33,38 @@ export default function Home() {
   } 
 
   return (
-    <div className="min-h-screen w-screen bg-white flex flex-col justify-center items-center gap-y-3">
+    <div className="!overflow-x-hidden px-4 min-h-screen w-screen bg-white flex flex-col justify-center items-center gap-y-3">
       <Aos />
 
-      <canvas
-        ref={canvasRef}
-        onMouseDown={onMouseDown}
-        width={820}
-        height={640}
-        className="px-3 md:px-0 bg-slate-50 border-2 border-slate-300 rounded-md"
-      />
+      <div className="mx-auto md:max-w-lg flex flex-col justify-start">
+        <h1 className="mb-4    text-4xl md:text-6xl font-bold leading-normal bg-clip-text text-transparent bg-gradient-to-b from-black to-slate-600" data-aos="zoom-out">
+          Hiyield Gamejam
+        </h1>
+        <span 
+          className="mb-4 text-2xl leading-normal"
+          data-aos="fade-right"
+          data-aos-delay="300"  
+        >
+          by Jake
+        </span>
+      </div>
 
-      <div className="flex flex-col" data-aos="fade-down">
+      <div className="z-10 relative px-3 md:px-0">
+        <canvas
+          ref={canvasRef}
+          onMouseDown={onMouseDown}
+          width={820}
+          height={640}
+          className="relative z-20 bg-slate-50 border-2 border-slate-300 rounded-md"
+        />
+        <div className="-z-50 absolute top-0 left-0 size-96 bg-blue-300 rounded-full filter blur-3xl opacity-9"></div>
+        <div className="-z-50 absolute bottom-0 left-0 size-96 bg-purple-300 rounded-full filter blur-3xl opacity-9"></div>
+
+        <div className="-z-50 absolute top-0 right-0 size-96 bg-orange-300 rounded-full filter blur-3xl opacity-9"></div>
+        <div className="-z-50 absolute bottom-0 right-0 size-96 bg-red-300 rounded-full filter blur-3xl opacity-9"></div>
+      </div>
+
+      <div className="flex flex-col" data-aos="fade-up" data-aos-delay="300">
         {isClient && <TwitterPicker color={color} onChange={(e) => setColor(e.hex)} />}
         <button 
           type="button" 
