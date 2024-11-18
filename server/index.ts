afcd16612171548df38941805f52ab50776b7cb1
Jake Mackie <jake@hiyield.co.uk>
2024-11-18 17:08:50 +0000

    feat: Connection logic

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 const express = require('express')
 const http = require('http')
+const chalk = require('chalk')
 const app = express()
 const server = http.createServer(app)
 
@@ -18,22 +19,32 @@ type DrawLine = {
   color: string
 }
 
+let players = 0;
+
 io.on('connection', (socket) => {
   socket.on('client-ready', () => {
-    socket.broadcast.emit('get-canvas-state')
-  })
+    console.log(`Client connected: ${socket.id}, total players: ${players}`);
+    players++;
+    io.emit('clients-count', players);
+    socket.broadcast.emit('get-canvas-state');
+  });
 
   socket.on('canvas-state', (state) => {
-    socket.broadcast.emit('canvas-state-from-server', state)
-  })
+    socket.broadcast.emit('canvas-state-from-server', state);
+  });
 
   socket.on('draw-line', ({ prevPoint, currentPoint, color }: DrawLine) => {
-    socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color })
-  })
+    socket.broadcast.emit('draw-line', { prevPoint, currentPoint, color });
+  });
 
-  socket.on('clear', () => io.emit('clear'))
-})
+  socket.on('clear', () => io.emit('clear'));
+
+  socket.on('disconnect', () => {
+    players = Math.max(players - 1, 0);
+    io.emit('clients-count', players);
+  });
+});
 
 server.listen(3001, () => {
-  console.log('✔️ Server listening on port 3001')
+  console.log(`${chalk.green('✓')} Server running on port 3001`)
 })
