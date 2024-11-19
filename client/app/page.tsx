6d5de629ee23f87fe1f49ae0db6496b3cfcf6f12
Jake Mackie <jake@hiyield.co.uk>
2024-11-19 16:34:28 +0000

    feat: Pretty neat pages

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center">
+    <div className="min-h-screen w-full flex flex-col gap-12 items-center justify-center">
+      <div className="flex">
+        <h1 className="text-6xl font-bold">
+          Canvas, but live.
+        </h1>
+      </div>
       <Link 
         href={`${process.env.CANONICAL_URL}/draw`}
-        className="text-xl hover:underline"
+        className="rounded-xl py-2 px-5 bg-blue-100 border-2 border-blue-300 text-blue-400"
       >
         Join drawing room
       </Link>
